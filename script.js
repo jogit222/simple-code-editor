98f6f19cd8b4a7e9a19076f7639cab3ba027af74
jogit222 <joris.bastiaens@leerling.busokristuskoning.be>
2026-01-27 10:40:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 const input = document.getElementById('input-area');
 const visual = document.getElementById('visual-layer');     
-console.log(synthaxDB)
-const eventColor = '#f58105'
+console.log(synthaxDB);
+const eventColor = '#f58105';
+const expressionColor = '#22c93b';
+const effectColor = '#102eb5';
+const conditionColor = '#e3b922';
+const structureColor = '#147d78';
+const sectionsColor = '#12b39a';
+const functionColor = '#a8a8a8';
+const itemColor = '#8f10b5';
+const miscColor = '#606361';
+
+
+function updateEditor()    {
+    const words = input.value.split(' ');
+    for (const item in words)   {
+        if (synthaxDB.events.contains('$item'))  {
+            visual.innerHTML += '<span style="color: ${eventColor}">${item}</span>';
+        }
+    }
+}
+
+
+
+
+
+input.addEventListener('keyup', updateEditor);
